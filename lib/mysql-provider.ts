9094505b82edf756bf565c771a72b63ff9d872a2
evolplus <evolplus@gmail.com>
2023-11-01 10:31:46 +0700

    Bug fixed: loadToken returns a TokenData instead of raw data.

--- lib/mysql-provider.ts
+++ lib/mysql-provider.ts
@@ -141,7 +141,7 @@ export class MySqlPassportModel implements PassportModel {
     async loadToken(userId: number, provider: string): Promise<TokenData | undefined> {
         let rs = await this.query(`SELECT * FROM ${TABLE_NAME_OAUTH} WHERE user_id=? AND provider=?`, [userId, provider]);
         if (rs && rs[0]) {
-            return rs[0] as TokenData;
+            return JSON.parse(rs[0].token) as TokenData;
         }
     }
 
